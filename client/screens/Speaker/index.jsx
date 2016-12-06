02e821c66893dc7a093c32279ca7facb0eb19a69
Leonardo Andres Garcia Crespo <leoasis@gmail.com>
2016-12-06 19:43:59 -0300

    Agrega pagination super sencilla con Relay

--- client/screens/Speaker/index.jsx
+++ client/screens/Speaker/index.jsx
@@ -4,23 +4,37 @@ import Relay from 'react-relay';
 import TalkList from 'shared/TalkList';
 
 
-const SpeakerScreen = ({ speaker }) => (
+const fetchMoreTalks = relay => (ev) => {
+  ev.preventDefault();
+  relay.setVariables({
+    count: relay.variables.count + 5
+  });
+};
+
+const SpeakerScreen = ({ speaker, relay }) => (
   <div>
     <h1>{speaker.firstName} {speaker.lastName}</h1>
     <p>{speaker.bio}</p>
     <h2>Charlas</h2>
     <TalkList talks={speaker.talks.edges.map(e => e.node)} />
+    {speaker.talks.pageInfo.hasNextPage &&
+      <button onClick={fetchMoreTalks(relay)}>Ver Más</button>
+    }
   </div>
 );
 
 export default Relay.createContainer(SpeakerScreen, {
+  initialVariables: { count: 5 },
   fragments: {
     speaker: () => Relay.QL`
       fragment on Speaker {
         firstName
         lastName
         bio
-        talks(first: 10) {
+        talks(first: $count) {
+          pageInfo {
+            hasNextPage
+          }
           edges {
             node {
               id
