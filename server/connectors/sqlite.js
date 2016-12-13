925ea881171b6c7f8321ff747b7f9dd1641999f8
Pablo Chiappetti <pchiappetti@gmail.com>
2016-12-13 02:22:29 -0300

    refactor del connector para poder usar sus funciones entre sí

--- server/connectors/sqlite.js
+++ server/connectors/sqlite.js
@@ -7,6 +7,45 @@ const Topic = require('../db/models/Topic');
 const { getPaginatedModel } = require('./util/pagination');
 const { connectionFromCollection, modelToJSON } = require('./util/relay');
 
+const getSpeakers = (args) => {
+  const query = qb => (args.query ? (
+    qb
+      .where('first_name', 'LIKE', `%${args.query}%`)
+      .orWhere('last_name', 'LIKE', `%${args.query}%`)
+  ) : false);
+
+  return getPaginatedModel(Speaker, args, query).then(
+    collection => connectionFromCollection(collection, args, 'Speaker')
+  );
+};
+
+const getTalks = (args) => {
+  const query = qb => (args.query ? (
+    qb
+      .where('title', 'LIKE', `%${args.query}%`)
+      .orWhere('description', 'LIKE', `%${args.query}%`)
+  ) : false);
+  const extraParams = { withRelated: ['event.eventSeries'] };
+
+  return getPaginatedModel(Talk, args, query, extraParams).then(
+    collection => connectionFromCollection(collection, args, 'Talk')
+  );
+};
+
+const getTopics = args => (
+  getPaginatedModel(Topic, args).then(
+    collection => connectionFromCollection(collection, args, 'Topic')
+  )
+);
+
+const getEvents = args => getPaginatedModel(Event, args).then(
+  collection => connectionFromCollection(collection, args, 'Event')
+);
+
+const getEventSeries = args => getPaginatedModel(EventSerie, args).then(
+  collection => connectionFromCollection(collection, args, 'EventSerie')
+);
+
 const getTalkById = id => Talk.query({ where: { id } }).fetch({
   withRelated: ['speakers', 'event.eventSeries', 'topics']
 }).then(modelToJSON).then(talk => (
@@ -16,67 +55,42 @@ const getTalkById = id => Talk.query({ where: { id } }).fetch({
   )
 ));
 
-module.exports = {
-  getSpeakers: (args) => {
-    const query = qb => (args.query ? (
-      qb
-        .where('first_name', 'LIKE', `%${args.query}%`)
-        .orWhere('last_name', 'LIKE', `%${args.query}%`)
-    ) : false);
+const updateTalkById = (id, payload) => (
+  Talk.forge({ id }).save(payload).then(() => getTalkById(id))
+);
 
-    return getPaginatedModel(Speaker, args, query).then(
-      collection => connectionFromCollection(collection, args, 'Speaker')
-    );
-  },
-
-  getTalks: (args) => {
-    const query = qb => (args.query ? (
-      qb
-        .where('title', 'LIKE', `%${args.query}%`)
-        .orWhere('description', 'LIKE', `%${args.query}%`)
-    ) : false);
-    const extraParams = { withRelated: ['event.eventSeries'] };
-
-    return getPaginatedModel(Talk, args, query, extraParams).then(
-      collection => connectionFromCollection(collection, args, 'Talk')
-    );
-  },
-
-  getTopics: args => (
-    getPaginatedModel(Topic, args).then(
-      collection => connectionFromCollection(collection, args, 'Topic')
-    )
-  ),
-
-  getEvents: args => getPaginatedModel(Event, args).then(
-    collection => connectionFromCollection(collection, args, 'Event')
-  ),
-
-  getEventSeries: args => getPaginatedModel(EventSerie, args).then(
-    collection => connectionFromCollection(collection, args, 'EventSerie')
-  ),
-
-  getTalkById,
-
-  updateTalkById: (id, payload) => (
-    Talk.forge({ id }).save(payload).then(() => getTalkById(id))
-  ),
-
-  getSpeakerById: id => (
-    Speaker.query({ where: { id } })
-      .fetch({ withRelated: ['talks.event.eventSeries'] })
-      .then(modelToJSON)
-  ),
-
-  getEventById: id => Event.query({ where: { id } }).fetch().then(modelToJSON),
-  getEventSeriesById: id => EventSerie.query({ where: { id } }).fetch().then(modelToJSON),
-  getTopicById: id => Topic.query({ where: { id } }).fetch().then(modelToJSON),
-  talkAdd: ({ title, description, eventId, speakerIds, topicIds }) => (
-    Talk.forge({ title, description, event_id: eventId }).save()
-    .then(
-      talk => talk.related('speakers').attach(speakerIds)
-      .then(() => talk.related('topics').attach(topicIds))
-    )
+const getSpeakerById = id => (
+  Speaker.query({ where: { id } })
+    .fetch({ withRelated: ['talks.event.eventSeries'] })
     .then(modelToJSON)
+);
+
+const getEventById = id => Event.query({ where: { id } }).fetch().then(modelToJSON);
+
+const getEventSeriesById = id => EventSerie.query({ where: { id } }).fetch().then(modelToJSON);
+
+const getTopicById = id => Topic.query({ where: { id } }).fetch().then(modelToJSON);
+
+const talkAdd = ({ title, description, eventId, speakerIds, topicIds }) => (
+  Talk.forge({ title, description, event_id: eventId }).save()
+  .then(
+    talk => talk.related('speakers').attach(speakerIds)
+    .then(() => talk.related('topics').attach(topicIds))
   )
+  .then(modelToJSON)
+);
+
+module.exports = {
+  getSpeakers,
+  getTalks,
+  getTopics,
+  getEvents,
+  getEventSeries,
+  getTalkById,
+  updateTalkById,
+  getSpeakerById,
+  getEventById,
+  getEventSeriesById,
+  getTopicById,
+  talkAdd
 };
